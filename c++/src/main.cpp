63a3a7e225e7ae5c0a5d9dc7c0314cdd9c1435a9
Khrisna Kamarga <khrisna.kamarga@freshconsulting.com>
2021-02-03 10:57:37 -0800

    added loop to the cpp debugging

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,9 +1,17 @@
 #include "person.h"
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
     Person khrisna("Khrisna", 23);
-    khrisna.introduce();
+    int count = 10;
+
+    while (count > 0)
+    {
+        khrisna.introduce();
+        sleep(5);
+        --count;
+    }
 
     return 0;
 }
